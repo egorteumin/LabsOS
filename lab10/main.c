e7b29b3de1f3a7299fae041d97049b244fe484b7
egorteumin <egorteumin@gmail.com>
2024-12-13 14:41:14 +0300

    fix

--- lab10/main.c
+++ lab10/main.c
@@ -5,15 +5,11 @@
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond_var = PTHREAD_COND_INITIALIZER;
-bool is_ready = false;
 
 void* read_arr(void* arg){
     while(1){
         pthread_mutex_lock(&mutex);
-        while(!is_ready){
-            pthread_cond_wait(&cond_var, &mutex);
-        }
-        is_ready = false;
+        pthread_cond_wait(&cond_var, &mutex);
         printf("tid: %lx\tarr: %s\n", pthread_self(), (char*)arg);
         pthread_mutex_unlock(&mutex);
     }
@@ -25,7 +21,6 @@ void* write_arr(void* arg){
     while(1){
         pthread_mutex_lock(&mutex);
         snprintf(arg, 32, "%ld", n++);
-        is_ready = true;
         pthread_cond_broadcast(&cond_var);
         pthread_mutex_unlock(&mutex);
         sleep(1);
